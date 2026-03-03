fe5f44d28a221e258896eafbb292f66c8f2505b7
santiagoc-wav <santitocardenas@gmail.com>
2026-03-03 18:51:46 +0000

    Añadi iconos y animaciones

--- script.js
+++ script.js
@@ -1,14 +1,47 @@
 const titulo = document.getElementById("titulo");
-const audio = new Audio("audios/Pad.mp3");
+const iconContainer = document.getElementById("iconContainer");
 
+// AUDIO INICIAL
+const audio = new Audio("audios/Pad.mp3");
 let isPlaying = false;
 
-function playAudio() {
+// Lista de iconos
+const iconos = [
+    "icons/icon1.png",
+    "icons/icon2.png",
+    "icons/icon3.png",
+    "icons/icon4.png",
+    "icons/icon5.png",
+    "icons/icon6.png",
+    "icons/icon7.png",
+    "icons/icon8.png"
+];
+
+let yaAparecieron = false;
+
+titulo.addEventListener("click", () => {
+
+    // reproducir audio pad
     if (!isPlaying) {
         audio.play();
         isPlaying = true;
     }
-}
 
-// click
-titulo.addEventListener("click", playAudio);
+    // poner los iconos
+    if (yaAparecieron) return;
+
+    iconos.forEach((ruta) => {
+
+        const img = document.createElement("img");
+        img.src = ruta;
+        img.classList.add("icon");
+
+        // Posición aleatoria
+        img.style.top = Math.random() * 80 + "%";
+        img.style.left = Math.random() * 80 + "%";
+
+        iconContainer.appendChild(img);
+    });
+
+    yaAparecieron = true;
+});
